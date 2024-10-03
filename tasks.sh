d91dbc0c05ec4eae071405e433a15578ab1305d3
Italo Silva <italo@maleldil.com>
2024-10-03 13:26:55 +0100

    tasks: Use printf instead of echo
    More portable, especially when it comes to escaped sequences. E.g.
    `echo -e "\na"` doesn't work on the default macOS echo.

--- tasks.sh
+++ tasks.sh
@@ -3,40 +3,40 @@
 UV_MIN_VERSION="0.4.0"
 
 usage() {
-	echo "Usage: $0 <task>"
-	echo "Run '$0 help' for a list of available tasks."
+	printf "Usage: %s <task>\n" "$0"
+	printf "Run '%s help' for a list of available tasks.\n" "$0"
 }
 
 help() {
-	echo "Usage: $0 <task>"
-	echo
-	echo "Available tasks:"
-	echo "  setup   Set up the development environment"
-	echo "  check   Run ruff check, ruff format, and pyright"
-	echo "  help    Show this help message"
+	printf "Usage: %s <task>\n" "$0"
+	printf "\n"
+	printf "Available tasks:\n"
+	printf "  setup   Set up the development environment\n"
+	printf "  check   Run ruff check, ruff format, and pyright\n"
+	printf "  help    Show this help message\n"
 }
 
 setup() {
 	if ! command -v uv >/dev/null 2>&1; then
-		echo "uv is not installed. Installing..."
+		printf "uv is not installed. Installing...\n"
 		curl -LsSf https://astral.sh/uv/install.sh | sh
 	else
 		# Check if uv is at least the minimum version, and upgrade if necessary
 		installed_version=$(uv --version | awk '{print $2}')
 		if [ "$(printf '%s\n' "$UV_MIN_VERSION" "$installed_version" | sort -V | head -n1)" != \
 			"$UV_MIN_VERSION" ]; then
-			echo "Installed uv is too old. Upgrading uv..."
+			printf "Installed uv is too old. Updating uv...\n"
 			uv self update
 		fi
 	fi
 
-	echo "Installing dependencies with uv..."
+	printf "\nInstalling dependencies with uv...\n"
 	uv sync
 
-	echo "Installing pre-commit hooks..."
+	printf "\nInstalling pre-commit hooks...\n"
 	uv run pre-commit install
 
-	echo "Setup complete. See README.md and CONTRIBUTING.md for more information."
+	printf "\nSetup complete. See README.md and CONTRIBUTING.md for more information.\n"
 }
 
 check() {
@@ -55,7 +55,7 @@ help | setup | check)
 	"$1"
 	;;
 *)
-	echo "Error: Unknown task '$1'"
+	printf "Error: Unknown task '%s'\n\n" "$1"
 	usage
 	exit 1
 	;;
