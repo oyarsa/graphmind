07e11ffa6d8af8a1e63710fa70e78a27ceec4cb3
Italo Silva <italo@maleldil.com>
2024-10-14 14:56:26 +0100

    asap(references): Add script to add reference abstracts
    Match the references in each ASAP paper with the results from the S2
    API, then use that to get the abstract.
    
    Uses a minimum fuzzy ratio of 80 (default, CLI setting). If no S2 papers
    match that, the reference is discarded.

---
+++ src/paper_hypergraph/asap/add_paper_references.py
@@ -0,0 +1,136 @@
+"""Match references from ASAP papers with S2 query results to obtain their abstracts.
+
+The goal is to retrieve the abstract for these references. This is done by querying
+the S2 API with the reference titles, taking their best match, and then doing our own
+fuzzy matching to ensure the titles are compatible.
+
+Note that this process doesn't ensure perfect matching, but it's the best we can do. By
+default, we use a minimum fuzzy ratio of 80%, which includes 90% of the S2 matches. If
+no papers from S2 clear that threshold for a given reference, the reference is removed.
+"""
+
+import argparse
+from collections.abc import Sequence
+from pathlib import Path
+
+from pydantic import BaseModel, ConfigDict, TypeAdapter
+from tqdm import tqdm
+
+from paper_hypergraph.asap.model import DatasetAdapter, PaperReference, PaperSection
+from paper_hypergraph.util import fuzzy_ratio
+
+
+class S2Paper(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    title_query: str
+    abstract: str
+
+
+class ReferenceWithAbstract(PaperReference):
+    """Standard ASAP reference with an added `abstract` field."""
+
+    abstract: str
+
+
+class PaperWithFullReference(BaseModel):
+    """Paper from ASAP where the references contain their abstract.
+
+    The abstract is retrieved by querying the S2 API with the reference title, then doing
+    fuzzy matching to ensure the returned paper is correct.
+    """
+
+    model_config = ConfigDict(frozen=True)
+
+    title: str
+    abstract: str
+    ratings: Sequence[int]
+    sections: Sequence[PaperSection]
+    approval: bool
+    references: Sequence[ReferenceWithAbstract]
+
+
+def _match_paper_external(
+    source: PaperReference, external_papers: Sequence[S2Paper], min_score: int
+) -> ReferenceWithAbstract | None:
+    """Find the external paper whose title best matches the source paper.
+
+    Match is done by fuzzy ratio. The highest ratio is chosen, as long as it's higher
+    than `min_score`.
+
+    Returns:
+        Best external S2 paper match. None if no papers' ratio is over `min_score`.
+    """
+    best_external = None
+    best_score = min_score
+
+    for external in external_papers:
+        ratio = fuzzy_ratio(source.title, external.title_query)
+        if ratio >= best_score:
+            best_external = external
+            best_score = ratio
+
+    if best_external is None:
+        return None
+
+    return ReferenceWithAbstract(
+        title=source.title,
+        year=source.year,
+        authors=source.authors,
+        contexts=source.contexts,
+        abstract=best_external.abstract,
+    )
+
+
+def add_references(
+    papers_file: Path,
+    external_files: Path,
+    output_file: Path,
+    min_score: int,
+    file_limit: int | None,
+) -> None:
+    source_papers = DatasetAdapter.validate_json(papers_file.read_text())[:file_limit]
+    external_papers = TypeAdapter(list[S2Paper]).validate_json(
+        external_files.read_bytes()
+    )
+
+    output = [
+        PaperWithFullReference(
+            title=paper.title,
+            abstract=paper.abstract,
+            ratings=paper.ratings,
+            sections=paper.sections,
+            approval=paper.approval,
+            references=[
+                match
+                for ref in paper.references
+                if (match := _match_paper_external(ref, external_papers, min_score))
+            ],
+        )
+        for paper in tqdm(source_papers)
+    ]
+    output_file.write_bytes(TypeAdapter(list[PaperWithFullReference]).dump_json(output))
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("papers", type=Path, help="Path to ASAP filtered papers file")
+    parser.add_argument("external", type=Path, help="Path to S2 reference papers file")
+    parser.add_argument("output", type=Path, help="Path to output JSON file")
+    parser.add_argument(
+        "--min-score",
+        type=int,
+        default=80,
+        help="Minimum fuzzy score to match an external paper",
+    )
+    parser.add_argument(
+        "--limit", "-n", type=int, default=None, help="Maximum source papers to process"
+    )
+    args = parser.parse_args()
+    add_references(args.papers, args.external, args.output, args.min_score, args.limit)
+
+
+if __name__ == "__main__":
+    main()
