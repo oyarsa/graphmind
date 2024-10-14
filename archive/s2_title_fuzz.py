3c8d6a77ef2fe3732d4e7cd9c834a02d81563088
Italo Silva <italo@maleldil.com>
2024-10-14 11:30:48 +0100

    archive: Change data structure from S2 title fuzz script
    Instead of creating another type of file with just title/query/fuzz,
    instead copy all the input data and just add the fuzz_ratio to it.
    
    In addition to that, accept a flag to filter the papers by a minimum
    fuzzy ratio, and create a separate file for the entries above that
    threshold.

--- archive/s2_title_fuzz.py
+++ archive/s2_title_fuzz.py
@@ -1,40 +1,27 @@
 """Calculate citation quality for the S2 API results through fuzzy matching scores.
 
 Takes as input the best matches from `paper_hypergraph.s2orc.retrieve_papers_semantic_scholar`
-and calculates the fuzzy ratio between the retrieved best paper tiel and the original
-title query.
+and calculates the fuzzy ratio between the retrieved best paper title and the original
+title query (`title` vs `title_query`).
 
-Saves the result as a JSON file containing array where each object represents a paper
-in the input file, with the following fields:
-- title_query (str): the original paper title used to query the S2 API
-- title_result (str): the title of the retrieved paper from the API
-- fuzz_ratio (int): number from 0-100 from fuzzy matching
+The saved result is identical to the input with an extra `fuzz_ratio` field.
+
+Also takes a parameter "min_fuzzy" that if present, creates an additional file with only
+entries with `fuzz_ratio` above that threshold. This file has the same name as the
+normal output file, but with `.filtered.json` as extension.
 
 This isn't necessary for Crossref because it already does the fuzzy matching there, so
-the output already contains the `fuzzy_ratio` field.
+the output already contains the `fuzzy_ratio` field. New versions of the Semantic Scholar
+retrieval script also do this.
 """
 
+import json
 from pathlib import Path
 from typing import Annotated
 
 import typer
-from pydantic import BaseModel, ConfigDict, TypeAdapter
 
 from paper_hypergraph.util import fuzzy_ratio
-
-
-class Paper(BaseModel):
-    model_config = ConfigDict(frozen=True)
-
-    title_query: str
-    title: str
-
-
-class Output(BaseModel):
-    title_query: str
-    title_result: str
-    fuzz_ratio: int
-
 
 app = typer.Typer(
     context_settings={"help_option_names": ["-h", "--help"]},
@@ -50,19 +37,23 @@ def main(
         typer.Argument(help="Path to input JSON file (semantic_scholar_best.json)."),
     ],
     output_file: Annotated[Path, typer.Argument(help="Path to output JSON file.")],
+    min_fuzzy: Annotated[
+        int | None, typer.Option(help="Minimum fuzzy ratio to filter")
+    ],
 ) -> None:
-    input_data = TypeAdapter(list[Paper]).validate_json(input_file.read_text())
-
+    input_data = json.loads(input_file.read_text())
     output_data = [
-        Output(
-            title_query=p.title_query,
-            title_result=p.title,
-            fuzz_ratio=fuzzy_ratio(p.title_query, p.title),
-        )
-        for p in input_data
+        paper | {"fuzz_ratio": fuzzy_ratio(paper["title_query"], paper["title"])}
+        for paper in input_data
     ]
+    output_file.write_text(json.dumps(output_data))
 
-    output_file.write_bytes(TypeAdapter(list[Output]).dump_json(output_data))
+    if min_fuzzy:
+        filtered_data = [
+            paper for paper in output_data if paper["fuzz_ratio"] >= min_fuzzy
+        ]
+        filtered_file = output_file.with_name(output_file.stem + ".filtered.json")
+        filtered_file.write_text(json.dumps(filtered_data, indent=2))
 
 
 if __name__ == "__main__":
