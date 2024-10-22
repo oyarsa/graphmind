37ea5781c40d0142c5c68c997612a57d92ea0aac
Italo Silva <italo@maleldil.com>
2024-10-22 14:00:26 +0100

    scripts(count_context): Replace context/polarity bash with Python
    I'm trying to minimise the number of bash/sh/fish scripts as much as
    possible because Python programs are way more maintanable and I just
    understand them better. With typer, they aren't even that verbose, and I
    can use the Pydantic models to make parsing/navigation explicit.

--- scripts/context_count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <filename>"
-	exit 1
-fi
-
-jq '[.[].references[].contexts[]] | length' "$1"
---
+++ scripts/count_context.py
@@ -0,0 +1,47 @@
+"""Count number of context items in data file. Show polarity frequencies, if available.
+
+The input file should have the `paper_hypergraph.asap.models.PaperWithReferenceEnriched`
+format.
+"""
+
+from collections import Counter
+from pathlib import Path
+from typing import Annotated
+
+import typer
+from pydantic import TypeAdapter
+
+from paper_hypergraph.asap.model import PaperWithReferenceEnriched
+
+app = typer.Typer(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    add_completion=False,
+    rich_markup_mode="rich",
+    pretty_exceptions_show_locals=False,
+)
+
+
+@app.command(help=__doc__)
+def main(
+    input_file: Annotated[Path, typer.Argument(help="Input JSON file.")],
+) -> None:
+    papers = TypeAdapter(list[PaperWithReferenceEnriched]).validate_json(
+        input_file.read_text()
+    )
+    contexts = [
+        context
+        for paper in papers
+        for reference in paper.references
+        for context in reference.contexts_annotated_valid
+    ]
+
+    print(f"{"contexts":<8} : {len(contexts)}")
+
+    if polarities := [p for context in contexts if (p := context.polarity)]:
+        print()
+        for key, value in Counter(polarities).most_common():
+            print(f"{key:<8} : {value}")
+
+
+if __name__ == "__main__":
+    app()
