30daf85d3fb4a7b9b0011136a2bf37ef69237a7c
Italo Silva <italo@maleldil.com>
2024-10-23 12:47:45 +0100

    scripts(explore_context): Don't print metrics - it's redundant
    When the input data contains the gold annotations, the "info" output
    will already include the metrics.
    
    Also, `show_classfied_stats` already prints the number of contexts, so I
    don't need to do that.

--- scripts/explore_context_results.py
+++ scripts/explore_context_results.py
@@ -33,20 +33,10 @@ def main(
     )
 
     papers = [result.item for result in input_data]
-    contexts = [
-        context
-        for paper in papers
-        for reference in paper.references
-        for context in reference.contexts
-    ]
+    print(f"Papers: {len(papers)}")
 
-    print(f"Papers  : {len(papers)}")
-    print(f"Contexts: {len(contexts)}")
-
-    info, metrics = show_classified_stats(papers)
+    info, _ = show_classified_stats(papers)
     print(info)
-    if metrics:
-        print(metrics.model_dump_json(indent=2))
 
 
 if __name__ == "__main__":
