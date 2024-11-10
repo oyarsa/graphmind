f3d5f195f2ad315bf30006fff4cf4f16ab02aa70
Italo Silva <italo@maleldil.com>
2024-11-10 02:21:35 +0000

    external(s2_areas,s2_model): Move API model types to separate module

--- src/paper/external_data/semantic_scholar_areas.py
+++ src/paper/external_data/semantic_scholar_areas.py
@@ -19,19 +19,20 @@ import tomllib
 from collections import defaultdict
 from collections.abc import Iterable, Sequence
 from pathlib import Path
-from typing import Annotated, Any
+from typing import Any
 
 import aiohttp
 import backoff
 import dotenv
 from aiolimiter import AsyncLimiter
-from pydantic import BaseModel, ConfigDict, Field, TypeAdapter
+from pydantic import BaseModel, ConfigDict, TypeAdapter
 from rich.console import Console
 from rich.table import Table
 from tqdm import tqdm
 
 from paper import progress
 from paper.external_data.semantic_scholar_info import S2_SEARCH_BASE_URL
+from paper.external_data.semantic_scholar_model import Paper
 from paper.util import (
     HelpOnErrorArgumentParser,
     arun_safe,
@@ -428,61 +429,6 @@ class AreaResult(BaseModel):
 
     area: str
     papers: Sequence[Paper]
-
-
-class Paper(BaseModel):
-    """Paper returned by the Semantic Scholar API. Everything's optional but `corpusId`.
-
-    This is to avoid validation errors in the middle of the download. We'll only save
-    those with non-empty `abstract`, though.
-    """
-
-    model_config = ConfigDict(frozen=True)
-
-    # Semantic Scholar's primary unique identifier for a paper.
-    paper_id: Annotated[str, Field(alias="paperId")]
-    # Semantic Scholar's secondary unique identifier for a paper.
-    corpus_id: Annotated[int | None, Field(alias="corpusId")]
-    # URL of the paper on the Semantic Scholar website.
-    url: str | None
-    # Title of the paper.
-    title: str | None
-    # The paper's abstract. Note that due to legal reasons, this may be missing even if
-    # we display an abstract on the website.
-    abstract: str | None
-    # The year the paper was published.
-    year: int | None
-    # The total number of papers this paper references.
-    reference_count: Annotated[int | None, Field(alias="referenceCount")]
-    # The total number of papers that reference this paper.
-    citation_count: Annotated[int | None, Field(alias="citationCount")]
-    # A subset of the citation count, where the cited publication has a significant
-    # impact on the citing publication.
-    influential_citation_count: Annotated[
-        int | None, Field(alias="influentialCitationCount")
-    ]
-    # The tldr paper summary.
-    tldr: Tldr | None
-    # Paper authors.
-    authors: Sequence[Author] | None
-
-
-class Tldr(BaseModel):
-    model_config = ConfigDict(frozen=True)
-
-    # The tldr model version number: https://github.com/allenai/scitldr
-    model: str | None
-    # The tldr paper summary.
-    text: str | None
-
-
-class Author(BaseModel):
-    model_config = ConfigDict(frozen=True)
-
-    # Author's name.
-    name: str | None
-    # Semantic Scholar's unique ID for the author.
-    author_id: Annotated[str | None, Field(alias="authorId")]
 
 
 class PaperOutput(Paper):
