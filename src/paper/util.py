0caaba3b7ccc5b856bc905ee181bce841480e587
Italo Silva <italo@maleldil.com>
2024-11-11 16:19:47 +0000

    paper: Add `use_alias` option to `load_data` and `save_data`
    This doesn't change the behaviour, only allows the user to change if
    required.
    
    By default now, both saving and loading will use aliases.

--- src/paper/util.py
+++ src/paper/util.py
@@ -19,7 +19,7 @@ from typing import Any, NoReturn, Protocol, Self, override
 
 import colorlog
 from aiolimiter import AsyncLimiter
-from pydantic import BaseModel, TypeAdapter
+from pydantic import BaseModel, ConfigDict, TypeAdapter
 from thefuzz import fuzz  # type: ignore
 
 
@@ -424,26 +424,36 @@ def arun_safe[**P, R](
     return run_safe(asyncio.run, async_func(*args, **kwargs))
 
 
-def load_data[T: BaseModel](path: Path, type_: type[T]) -> list[T]:
+def load_data[T: BaseModel](
+    path: Path, type_: type[T], use_alias: bool = True
+) -> list[T]:
     """Load a list of data from JSON file in `path`.
 
     Args:
         path: File to read the data from. Must be a list of objects.
         type_: Type of the objects in the list.
+        use_alias: If True, read object keys by using the real field names, not aliases.
 
     Returns:
         List of data with the `type_` format.
     """
-    return TypeAdapter(list[type_]).validate_json(path.read_bytes())
+    return TypeAdapter(
+        list[type_], config=ConfigDict(populate_by_name=not use_alias)
+    ).validate_json(path.read_bytes())
 
 
-def save_data[T: BaseModel](path: Path, data: Sequence[T]) -> None:
+def save_data[T: BaseModel](
+    path: Path, data: Sequence[T], use_alias: bool = True
+) -> None:
     """Save sequence of data in a JSON file at `path`.
 
     Args:
         path: File where data will be saved. Creates its parent directory if it doesn't
             exist.
         data: The data to be saved. Must be non-empty.
+        use_alias: If True, the output object keys will use the field alias, not the
+            actual field name. Defaults to True because Pydantic will use the alias
+            when reading.
     """
     if not data:
         raise ValueError("Cannot save empty data")
@@ -451,7 +461,7 @@ def save_data[T: BaseModel](path: Path, data: Sequence[T]) -> None:
     path.parent.mkdir(parents=True, exist_ok=True)
     type_ = type(data[0])
     path.write_bytes(
-        TypeAdapter(Sequence[type_]).dump_json(data, indent=2, by_alias=True)
+        TypeAdapter(Sequence[type_]).dump_json(data, indent=2, by_alias=use_alias)
     )
 
 
