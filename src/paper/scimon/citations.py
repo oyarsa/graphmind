56be4b39b8c4156b6561d6dc45b9f80c0e2a5788
Italo Silva <italo@maleldil.com>
2024-11-20 19:39:38 +0000

    scimon(citations): Speed up embedding and similarity
    Use the parallel implementation when encoding multiple titles and
    calculate the similarities by matrix/vector multiplication.

--- src/paper/scimon/citations.py
+++ src/paper/scimon/citations.py
@@ -1,28 +1,28 @@
 """Filter paper citation graph by title similarity, keeping top K only.
 
 Calculates title sentence embedding for the ASAP main paper and each S2 reference using
-SentenceTransformer, then keep the top K most similar per paper. SciMON uses the
+a SentenceTransformer, then keep the top K most similar per paper. SciMON uses the
 `all-mpnet-base-v2` model and K = 5.
 
-Takes as input the output of `external_data.semantic_scholar.construct_daset`, the file
+Takes as input the output of `external_data.semantic_scholar.construct_daset`: the file
 `asap_with_s2_references.json` of type `s2.ASAPWithFullS2`. The similarity is calculated
 between the ASAP `title` and the S2 `title_query`.
 """
 
 from __future__ import annotations
 
-from collections.abc import Iterable, Sequence
+import os
+from collections.abc import Iterable
 from pathlib import Path
-from typing import Annotated, Self, cast
+from typing import Annotated, Any, Literal, Self, cast
 
 import numpy as np
 import numpy.typing as npt
 import typer
-from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 
-import paper.asap.model as asap
 import paper.external_data.semantic_scholar.model as s2
+from paper.util import display_params
 from paper.util.serde import load_data, save_data
 
 app = typer.Typer(
@@ -51,10 +51,12 @@ def main(
         str, typer.Option("--model", help="SentenceTransformer model to use.")
     ] = "all-mpnet-base-v2",
 ) -> None:
+    print(display_params())
+
     asap_papers = load_data(input_file, s2.ASAPWithFullS2)
 
-    encoder = TitleEncoder(model_name)
-    asap_topk_refs = _keep_top_k_titles(asap_papers, encoder, k)
+    with TitleEncoder(model_name) as encoder:
+        asap_topk_refs = _keep_top_k_titles(asap_papers, encoder, k)
 
     save_data(output_file, asap_topk_refs)
 
@@ -64,8 +66,8 @@ def _keep_top_k_titles(
 ) -> list[s2.ASAPWithFullS2]:
     """For each ASAP paper, keep top K references by title embedding similarity.
 
-    Compares the ASAP `title` with the S2 `title_query`. Cleans up the titles with
-    `s2.clean_title` first.
+    Cleans up the titles with `s2.clean_title`, then compares the ASAP `title` with the
+    S2 `title_query`.
 
     The output has the same format as the input, except with fewer (top K) references.
     """
@@ -74,85 +76,116 @@ def _keep_top_k_titles(
     for asap_paper in tqdm(asap_papers, desc="Processing ASAP papers"):
         asap_embedding = encoder.encode(asap_paper.title)
 
-        s2_references = [
-            S2PaperEncoded.from_(
-                s2_ref,
-                asap_paper.title,
-                _similarity(asap_embedding, encoder.encode(s2_ref.title_query)),
-            )
-            for s2_ref in asap_paper.references
-        ]
+        s2_embeddings = encoder.encode_multi(
+            s2.title_query for s2 in asap_paper.references
+        )
+        s2_similarities = _similarities(asap_embedding, s2_embeddings)
 
-        s2_top_k = sorted(s2_references, key=lambda x: x.similarity, reverse=True)[:k]
-        output.append(asap_paper.model_copy(update={"references": s2_top_k}))
+        s2_top_k = sorted(
+            zip(asap_paper.references, s2_similarities),
+            key=lambda x: x[1],
+            reverse=True,
+        )[:k]
+
+        output.append(
+            asap_paper.model_copy(update={"references": [ref for ref, _ in s2_top_k]})
+        )
 
     return output
 
 
-class S2PaperEncoded(asap.S2Paper):
-    """Same as the S2 referenced papers from ASAP, but with the source and similarity.
-
-    Attributes:
-        source_title: Title of the paper that cited this.
-        similarity: Cosine similarity between the `source_title` and this `title_query`.
-    """
-
-    source_title: str
-    similarity: float
-
-    @classmethod
-    def from_(cls, paper: asap.S2Paper, source_title: str, similarity: float) -> Self:
-        return cls(
-            title_query=paper.title_query,
-            title=paper.title,
-            paperId=paper.paper_id,
-            corpusId=paper.corpus_id,
-            url=paper.url,
-            abstract=paper.abstract,
-            year=paper.year,
-            referenceCount=paper.reference_count,
-            citationCount=paper.citation_count,
-            influentialCitationCount=paper.influential_citation_count,
-            tldr=paper.tldr,
-            authors=paper.authors,
-            source_title=source_title,
-            similarity=similarity,
-        )
-
-
 type TitleEmbedding = npt.NDArray[np.float32]
+type TitleEmbeddingMatrix = npt.NDArray[np.float32]
+type SentenceTransformerPool = dict[Literal["input", "output", "processes"], Any]
 
 
 class TitleEncoder:
     def __init__(self, model_name: str) -> None:
+        # `sentence_transformers` has a bug where they don't clean up their semaphores
+        # properly, so we suppress this.
+        _hard_suppress_warning("multiprocessing.resource_tracker", "UserWarning")
+        from sentence_transformers import SentenceTransformer
+
         self._model = SentenceTransformer(model_name)
-        # Cache of `title_clean` -> `embedding`.
-        self._cache: dict[str, TitleEmbedding] = {}
+
+        # Used for parallel processing.
+        self._pool = self._model.start_multi_process_pool()
 
     def encode(self, title_raw: str) -> TitleEmbedding:
-        """Clean and encode title as a vector using a `SentenceTransformer`.
+        """Clean and encode title as a vector.
 
-        Titles are cleaned-up with `s2.clean_title` first. Keeps a cache of vectors by
-        the title name to avoid recomputing the same data.
+        Title is cleaned-up with `s2.clean_title` first.
         """
         title_clean = s2.clean_title(title_raw)
-        if (embedding := self._cache.get(title_clean)) is not None:
-            return embedding
+        return cast(TitleEmbedding, self._model.encode(title_clean))  # type: ignore
 
-        embedding = cast(TitleEmbedding, self._model.encode(title_clean))  # type: ignore
-        self._cache[title_clean] = embedding
-        return embedding
+    def encode_multi(self, titles_raw: Iterable[str]) -> TitleEmbeddingMatrix:
+        """Clean and parallel encode multiple titles as vectors.
+
+        Titles are cleaned-up with `s2.clean_title` first.
+        """
+        titles_clean = [s2.clean_title(title_raw) for title_raw in titles_raw]
+
+        embeddings = self._model.encode_multi_process(titles_clean, self._pool)  # type: ignore
+        return cast(TitleEmbeddingMatrix, embeddings)
+
+    def __enter__(self) -> Self:
+        """Start multiprocessing pool."""
+        return self
+
+    def __exit__(self, *_) -> None:
+        """Close multiprocessing pool."""
+        self._model.stop_multi_process_pool(self._pool)
 
 
-def _similarity(v1: TitleEmbedding, v2: TitleEmbedding) -> float:
-    """Calculate cosine similarity between two NumPy vectors.
+def _similarities(
+    vector: TitleEmbedding, matrix: TitleEmbeddingMatrix
+) -> npt.NDArray[np.float32]:
+    """Calculate cosine similarity between a `vector` and a `matrix`.
 
-    They both must be 1-dimensional and have the same number of items.
+    The `vector` will have shape (dim,), and matrices (N, dim), where N is the number of
+    entries. The output should be a vector of floats with N elements.
+
+    Raises:
+        ValueError: if vector or matrix have the wrong shape.
     """
-    # Ensure inputs are 1D
-    assert v1.ndim == 1 and v2.ndim == 1, "Input arrays must be 1-dimensional"
-    assert v1.shape[0] == v2.shape[0], "Input vectors must have same dimensionality"
+    # Preconditions
+    if vector.ndim != 1:
+        raise ValueError("vector must be 1D")
+    if matrix.ndim != 2:
+        raise ValueError("matrix must be 2D")
+    if vector.shape[0] != matrix.shape[1]:
+        raise ValueError("vector and matrix dimensions must be compatible")
 
-    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+    dot_product = np.dot(matrix, vector)
+
+    # Compute the L2 norms (magnitudes)
+    vector_norm = np.linalg.norm(vector)
+    matrix_norms = np.linalg.norm(matrix, axis=1)
+
+    # Avoid division by zero by adding a small epsilon
+    epsilon = 1e-8
+    denominator = (vector_norm * matrix_norms) + epsilon
+
+    similarities = (dot_product / denominator).astype(np.float32)
+
+    # Postconditions
+    assert similarities.ndim == 1, "similarities should be 1D"
+    assert (
+        similarities.shape[0] == matrix.shape[0]
+    ), "similarities must have as many elements as matrix rows"
+
+    return similarities
+
+
+def _hard_suppress_warning(name: str, type: str) -> None:
+    """Suppress warning by adding it to `PYTHONWARNINGS`."""
+    old_warnings = os.environ.get("PYTHONWARNINGS")
+    new_filter = f"ignore::{type}:{name}"
+    os.environ["PYTHONWARNINGS"] = (
+        f"{old_warnings},{new_filter}" if old_warnings else new_filter
+    )
+
+
 if __name__ == "__main__":
     app()
