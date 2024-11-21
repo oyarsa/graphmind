091d71465e78c5d16752be47f9dbd3e17c616c99
Italo Silva <italo@maleldil.com>
2024-11-21 18:46:22 +0000

    scimon(citations): Use new `embedidng.Encoder`
    The `TitleEncoder` wraps the `embedding.Encoder` by preprocessing the
    title before passing it to the encoder.

--- src/paper/scimon/citations.py
+++ src/paper/scimon/citations.py
@@ -11,17 +11,15 @@ between the ASAP `title` and the S2 `title_query`.
 
 from __future__ import annotations
 
-import os
 from collections.abc import Iterable
 from pathlib import Path
-from typing import Annotated, Any, Literal, Self, cast
+from typing import Annotated, Any, Self
 
-import numpy as np
-import numpy.typing as npt
 import typer
 from tqdm import tqdm
 
 import paper.external_data.semantic_scholar.model as s2
+from paper.scimon import embedding as emb
 from paper.util import display_params
 from paper.util.serde import load_data, save_data
 
@@ -79,7 +77,7 @@ def _keep_top_k_titles(
         s2_embeddings = encoder.encode_multi(
             s2.title_query for s2 in asap_paper.references
         )
-        s2_similarities = _similarities(asap_embedding, s2_embeddings)
+        s2_similarities = emb.similarities(asap_embedding, s2_embeddings)
 
         s2_top_k = sorted(
             zip(asap_paper.references, s2_similarities),
@@ -94,97 +92,34 @@ def _keep_top_k_titles(
     return output
 
 
-type TitleEmbedding = npt.NDArray[np.float32]
-type TitleEmbeddingMatrix = npt.NDArray[np.float32]
-type SentenceTransformerPool = dict[Literal["input", "output", "processes"], Any]
-
-
 class TitleEncoder:
     def __init__(self, model_name: str) -> None:
-        # `sentence_transformers` has a bug where they don't clean up their semaphores
-        # properly, so we suppress this.
-        _hard_suppress_warning("multiprocessing.resource_tracker", "UserWarning")
-        from sentence_transformers import SentenceTransformer
+        self._encoder = emb.Encoder(model_name)
+        self._encoder.__enter__()
 
-        self._model = SentenceTransformer(model_name)
-
-        # Used for parallel processing.
-        self._pool = self._model.start_multi_process_pool()
-
-    def encode(self, title_raw: str) -> TitleEmbedding:
+    def encode(self, title_raw: str) -> emb.Vector:
         """Clean and encode title as a vector.
 
         Title is cleaned-up with `s2.clean_title` first.
         """
-        title_clean = s2.clean_title(title_raw)
-        return cast(TitleEmbedding, self._model.encode(title_clean))  # type: ignore
+        return self._encoder.encode(s2.clean_title(title_raw))
 
-    def encode_multi(self, titles_raw: Iterable[str]) -> TitleEmbeddingMatrix:
+    def encode_multi(self, titles_raw: Iterable[str]) -> emb.Matrix:
         """Clean and parallel encode multiple titles as vectors.
 
         Titles are cleaned-up with `s2.clean_title` first.
         """
-        titles_clean = [s2.clean_title(title_raw) for title_raw in titles_raw]
-
-        embeddings = self._model.encode_multi_process(titles_clean, self._pool)  # type: ignore
-        return cast(TitleEmbeddingMatrix, embeddings)
+        return self._encoder.encode_multi(
+            [s2.clean_title(title_raw) for title_raw in titles_raw]
+        )
 
     def __enter__(self) -> Self:
-        """Start multiprocessing pool."""
+        """Start encoder's context."""
         return self
 
-    def __exit__(self, *_) -> None:
-        """Close multiprocessing pool."""
-        self._model.stop_multi_process_pool(self._pool)
-
-
-def _similarities(
-    vector: TitleEmbedding, matrix: TitleEmbeddingMatrix
-) -> npt.NDArray[np.float32]:
-    """Calculate cosine similarity between a `vector` and a `matrix`.
-
-    The `vector` will have shape (dim,), and matrices (N, dim), where N is the number of
-    entries. The output should be a vector of floats with N elements.
-
-    Raises:
-        ValueError: if vector or matrix have the wrong shape.
-    """
-    # Preconditions
-    if vector.ndim != 1:
-        raise ValueError("vector must be 1D")
-    if matrix.ndim != 2:
-        raise ValueError("matrix must be 2D")
-    if vector.shape[0] != matrix.shape[1]:
-        raise ValueError("vector and matrix dimensions must be compatible")
-
-    dot_product = np.dot(matrix, vector)
-
-    # Compute the L2 norms (magnitudes)
-    vector_norm = np.linalg.norm(vector)
-    matrix_norms = np.linalg.norm(matrix, axis=1)
-
-    # Avoid division by zero by adding a small epsilon
-    epsilon = 1e-8
-    denominator = (vector_norm * matrix_norms) + epsilon
-
-    similarities = (dot_product / denominator).astype(np.float32)
-
-    # Postconditions
-    assert similarities.ndim == 1, "similarities should be 1D"
-    assert (
-        similarities.shape[0] == matrix.shape[0]
-    ), "similarities must have as many elements as matrix rows"
-
-    return similarities
-
-
-def _hard_suppress_warning(name: str, type: str) -> None:
-    """Suppress warning by adding it to `PYTHONWARNINGS`."""
-    old_warnings = os.environ.get("PYTHONWARNINGS")
-    new_filter = f"ignore::{type}:{name}"
-    os.environ["PYTHONWARNINGS"] = (
-        f"{old_warnings},{new_filter}" if old_warnings else new_filter
-    )
+    def __exit__(self, *args: Any) -> None:
+        """Close encoder's context."""
+        return self._encoder.__exit__(*args)
 
 
 if __name__ == "__main__":
