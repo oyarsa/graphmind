7705565aed1363d3b8599ccae117caf0997c360c
Italo Silva <italo@maleldil.com>
2024-11-24 21:28:21 +0000

    scimon(citations): Citation graph is now paper ID -> cited titles
    I don't need the full paper, so instead I now have a mapping between the
    paper ID (`id` property) and the top K citations. For good measure, I'm
    returning both ASAP titles and S2 titles and paper IDs.

--- src/paper/scimon/citations.py
+++ src/paper/scimon/citations.py
@@ -1,4 +1,4 @@
-"""Filter paper citation graph by title similarity, keeping top K only.
+"""Create citations graph with the top K titles from cited papers.
 
 Calculates title sentence embedding for the ASAP main paper and each S2 reference using
 a SentenceTransformer, then keep the top K most similar per paper. SciMON uses the
@@ -11,17 +11,22 @@ between the ASAP `title` and the S2 `title_query`.
 
 from __future__ import annotations
 
-from collections.abc import Iterable
+import logging
+from collections.abc import Iterable, Mapping, Sequence
 from pathlib import Path
 from typing import Annotated
 
 import typer
+from openai import BaseModel
+from pydantic import ConfigDict, computed_field
 from tqdm import tqdm
 
 import paper.external_data.semantic_scholar.model as s2
 from paper.scimon import embedding as emb
 from paper.util import display_params
-from paper.util.serde import load_data, save_data
+from paper.util.serde import Record, load_data
+
+logger = logging.getLogger(__name__)
 
 app = typer.Typer(
     context_settings={"help_option_names": ["-h", "--help"]},
@@ -49,27 +54,25 @@ def main(
         str, typer.Option("--model", help="SentenceTransformer model to use.")
     ] = "all-mpnet-base-v2",
 ) -> None:
-    print(display_params())
+    logger.info(display_params())
 
     asap_papers = load_data(input_file, s2.ASAPWithFullS2)
 
     with emb.Encoder(model_name) as encoder:
-        asap_topk_refs = keep_to_k(asap_papers, encoder, k)
+        graph = keep_top_k_titles(asap_papers, encoder, k)
 
-    save_data(output_file, asap_topk_refs)
+    output_file.write_text(graph.model_dump_json(indent=2))
 
 
-def keep_to_k(
+def keep_top_k_titles(
     asap_papers: Iterable[s2.ASAPWithFullS2], encoder: emb.Encoder, k: int
-) -> list[s2.ASAPWithFullS2]:
-    """For each ASAP paper, keep top K references by title embedding similarity.
+) -> Graph:
+    """For each ASAP paper, find top K reference titles embedding similarity.
 
     Cleans up the titles with `s2.clean_title`, then compares the ASAP `title` with the
     S2 `title_query`.
-
-    The output has the same format as the input, except with fewer (top K) references.
     """
-    output: list[s2.ASAPWithFullS2] = []
+    paperid_to_cited: dict[int, list[Citation]] = {}
 
     for asap_paper in tqdm(asap_papers, desc="Processing ASAP papers"):
         asap_embedding = encode_title(encoder, asap_paper.title)
@@ -85,11 +88,41 @@ def keep_to_k(
             reverse=True,
         )[:k]
 
-        output.append(
-            asap_paper.model_copy(update={"references": [ref for ref, _ in s2_top_k]})
-        )
+        paperid_to_cited[asap_paper.id] = [
+            Citation(
+                title_s2=paper.title,
+                title_asap=paper.title_query,
+                paper_id=paper.paper_id,
+            )
+            for paper, _ in s2_top_k
+        ]
 
-    return output
+    return Graph(edge_list=paperid_to_cited)
+
+
+class Graph(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    edge_list: Mapping[int, Sequence[Citation]]
+    """Mapping of ASAP paper `id` to list of cited papers."""
+
+    @computed_field
+    @property
+    def nodes(self) -> Sequence[int]:
+        return sorted(self.edge_list)
+
+    def query(self, paper_id: int) -> Sequence[Citation]:
+        return self.edge_list[paper_id]
+
+
+class Citation(Record):
+    title_s2: str
+    title_asap: str
+    paper_id: str
+
+    @property
+    def id(self) -> str:
+        return self.paper_id
 
 
 def encode_title(encoder: emb.Encoder, title_raw: str) -> emb.Vector:
