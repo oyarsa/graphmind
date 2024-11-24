29cf0e00b1e400b27e47c0f8334e8633c0080e4d
Italo Silva <italo@maleldil.com>
2024-11-24 21:54:56 +0000

    scimon(citations): Keep all papers and do top K at query time
    I have to calculate the embeddings for all papers anyway, so what's the
    harm in keeping all of them?

--- src/paper/scimon/citations.py
+++ src/paper/scimon/citations.py
@@ -1,7 +1,8 @@
-"""Create citations graph with the top K titles from cited papers.
+"""Create citations graph with the reference papers sorted by title similarity.
 
 Calculates title sentence embedding for the ASAP main paper and each S2 reference using
-a SentenceTransformer, then keep the top K most similar per paper. SciMON uses the
+a SentenceTransformer, then keeping a sorted list by similarity. At query time, the user
+can specify the number of most similar papers to retrieve. SciMON uses the
 `all-mpnet-base-v2` model and K = 5.
 
 Takes as input the output of `external_data.semantic_scholar.construct_daset`: the file
@@ -14,11 +15,11 @@ from __future__ import annotations
 import logging
 from collections.abc import Iterable, Mapping, Sequence
 from pathlib import Path
-from typing import Annotated
+from typing import Annotated, Self
 
 import typer
 from openai import BaseModel
-from pydantic import ConfigDict, computed_field
+from pydantic import ConfigDict
 from tqdm import tqdm
 
 import paper.external_data.semantic_scholar.model as s2
@@ -49,7 +50,6 @@ def main(
             help="File with ASAP papers with top K references with full S2 data."
         ),
     ],
-    k: Annotated[int, typer.Option(help="Top K items to keep.")] = 5,
     model_name: Annotated[
         str, typer.Option("--model", help="SentenceTransformer model to use.")
     ] = "all-mpnet-base-v2",
@@ -59,86 +59,68 @@ def main(
     asap_papers = load_data(input_file, s2.ASAPWithFullS2)
 
     with emb.Encoder(model_name) as encoder:
-        graph = keep_top_k_titles(asap_papers, encoder, k)
+        graph = Graph.from_papers(encoder, asap_papers)
 
     output_file.write_text(graph.model_dump_json(indent=2))
-
-
-def keep_top_k_titles(
-    asap_papers: Iterable[s2.ASAPWithFullS2], encoder: emb.Encoder, k: int
-) -> Graph:
-    """For each ASAP paper, find top K reference titles embedding similarity.
-
-    Cleans up the titles with `s2.clean_title`, then compares the ASAP `title` with the
-    S2 `title_query`.
-    """
-    paperid_to_cited: dict[int, list[Citation]] = {}
-
-    for asap_paper in tqdm(asap_papers, desc="Processing ASAP papers"):
-        asap_embedding = encode_title(encoder, asap_paper.title)
-
-        s2_embeddings = encode_titles(
-            encoder, (s2.title_query for s2 in asap_paper.references)
-        )
-        s2_similarities = emb.similarities(asap_embedding, s2_embeddings)
-
-        s2_top_k = sorted(
-            zip(asap_paper.references, s2_similarities),
-            key=lambda x: x[1],
-            reverse=True,
-        )[:k]
-
-        paperid_to_cited[asap_paper.id] = [
-            Citation(
-                title_s2=paper.title,
-                title_asap=paper.title_query,
-                paper_id=paper.paper_id,
-            )
-            for paper, _ in s2_top_k
-        ]
-
-    return Graph(edge_list=paperid_to_cited)
 
 
 class Graph(BaseModel):
     model_config = ConfigDict(frozen=True)
 
+    nodes: Sequence[int]
+    """Nodes are the ASAP paper `id`s."""
     edge_list: Mapping[int, Sequence[Citation]]
     """Mapping of ASAP paper `id` to list of cited papers."""
 
-    @computed_field
-    @property
-    def nodes(self) -> Sequence[int]:
-        return sorted(self.edge_list)
+    @classmethod
+    def from_papers(
+        cls, encoder: emb.Encoder, asap_papers: Iterable[s2.ASAPWithFullS2]
+    ) -> Self:
+        """For each ASAP paper, sort cited papers by title similarity.
 
-    def query(self, paper_id: int) -> Sequence[Citation]:
-        return self.edge_list[paper_id]
+        Cleans up the titles with `s2.clean_title`, then compares the ASAP `title` with
+        the S2 `title_query`.
+        """
+        id_to_cited: dict[int, list[Citation]] = {}
+
+        for asap_paper in tqdm(asap_papers, desc="Processing ASAP papers"):
+            asap_embedding = encoder.encode(s2.clean_title(asap_paper.title))
+
+            s2_embeddings = encoder.encode_multi(
+                [s2.clean_title(r.title_query) for r in asap_paper.references]
+            )
+            s2_similarities = emb.similarities(asap_embedding, s2_embeddings)
+
+            id_to_cited[asap_paper.id] = [
+                Citation(
+                    title_s2=paper.title,
+                    title_asap=paper.title_query,
+                    paper_id=paper.paper_id,
+                    score=score,
+                )
+                for paper, score in sorted(
+                    zip(asap_paper.references, s2_similarities),
+                    key=lambda x: x[1],
+                    reverse=True,
+                )
+            ]
+
+        return cls(edge_list=id_to_cited, nodes=sorted(id_to_cited))
+
+    def query(self, paper_id: int, k: int) -> Sequence[Citation]:
+        """Get top K reference papers by title similarity from an ASAP paper `id`."""
+        return self.edge_list[paper_id][:k]
 
 
 class Citation(Record):
     title_s2: str
     title_asap: str
     paper_id: str
+    score: float
 
     @property
     def id(self) -> str:
         return self.paper_id
-
-
-def encode_title(encoder: emb.Encoder, title_raw: str) -> emb.Vector:
-    """Clean and encode title as a vector.
-
-    Title is cleaned-up with `s2.clean_title` first.
-    """
-    return encoder.encode(s2.clean_title(title_raw))
-
-
-def encode_titles(encoder: emb.Encoder, titles_raw: Iterable[str]) -> emb.Matrix:
-    """Clean and parallel encode multiple titles as vectors.
-
-    Titles are cleaned-up with `s2.clean_title` first.
-    """
-    return encoder.encode_multi([s2.clean_title(title_raw) for title_raw in titles_raw])
 
 
 if __name__ == "__main__":
