f2aa2276b95d2517973f6ca09eb769afaeb127b3
Italo Silva <italo@maleldil.com>
2024-11-27 14:00:07 +0000

    util(serde): Allow `load_data` to return single or list
    This one required more effort because we need to get the return type
    right, so I used overloads to ensure that the user doesn't have to worry
    about type assertions, casting or anything like that.
    
    `overload` is quite handy, but a little difficult to wrap your head
    around.

--- src/paper/util/serde.py
+++ src/paper/util/serde.py
@@ -3,6 +3,7 @@
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
 from pathlib import Path
+from typing import Literal, overload
 
 from pydantic import BaseModel, ConfigDict, TypeAdapter
 
@@ -20,31 +21,51 @@ class Record(BaseModel, ABC):
     def id(self) -> str: ...
 
 
+@overload
 def load_data[T: BaseModel](
-    path: Path, type_: type[T], use_alias: bool = True
-) -> list[T]:
-    """Load a list of data from JSON file in `path`.
+    file: Path, type_: type[T], use_alias: bool = True, *, single: Literal[True]
+) -> T: ...
+
+
+@overload
+def load_data[T: BaseModel](
+    file: Path, type_: type[T], use_alias: bool = True, single: Literal[False] = False
+) -> list[T]: ...
+
+
+def load_data[T: BaseModel](
+    file: Path,
+    type_: type[T],
+    use_alias: bool = True,
+    single: bool = False,
+) -> list[T] | T:
+    """Load data from the JSON `file`: a single object (if `single=True`) or a list.
 
     Args:
-        path: File to read the data from. Must be a list of objects.
+        file: File to read the data from. Must be a list of objects.
         type_: Type of the objects in the list.
         use_alias: If True, read object keys by using the real field names, not aliases.
+        single: If True, return a single object. If False, return a list of objects.
 
     Returns:
         List of data with the `type_` format.
     """
+    content = file.read_bytes()
+    if single:
+        return type_.model_validate_json(content)
+
     return TypeAdapter(
         list[type_], config=ConfigDict(populate_by_name=not use_alias)
-    ).validate_json(path.read_bytes())
+    ).validate_json(content)
 
 
 def save_data[T: BaseModel](
-    path: Path, data: Sequence[T] | T, use_alias: bool = True
+    file: Path, data: Sequence[T] | T, use_alias: bool = True
 ) -> None:
-    """Save data in JSON file at `path`. Can be a single Pydantic object or a Sequence.
+    """Save data in JSON `file`. Can be a single Pydantic object or a Sequence.
 
     Args:
-        path: File where data will be saved. Creates its parent directory if it doesn't
+        file: File where data will be saved. Creates its parent directory if it doesn't
             exist.
         data: The data to be saved. If it's a sequence, it must be non-empty.
         use_alias: If True, the output object keys will use the field alias, not the
@@ -53,11 +74,11 @@ def save_data[T: BaseModel](
     if not data:
         raise ValueError("Cannot save empty data")
 
-    path.parent.mkdir(parents=True, exist_ok=True)
+    file.parent.mkdir(parents=True, exist_ok=True)
     if isinstance(data, Sequence):
         type_ = type(data[0])
-        path.write_bytes(
+        file.write_bytes(
             TypeAdapter(Sequence[type_]).dump_json(data, indent=2, by_alias=use_alias)
         )
     else:
-        path.write_text(data.model_dump_json(indent=2, by_alias=use_alias))
+        file.write_text(data.model_dump_json(indent=2, by_alias=use_alias))
