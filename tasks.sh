b16310b0fddef3ff22e9d49ea6da9489aed226ac
Italo Silva <italo@maleldil.com>
2024-12-04 22:41:03 +0000

    Remove sourcery from deps and tasks
    The CLI isn't that useful. I'd rather just have the LSP in the editor.

--- tasks.sh
+++ tasks.sh
@@ -15,7 +15,6 @@ help() {
 	printf "  lint    Run ruff check, ruff format, and pyright\n"
 	printf "  help    Show this help message\n"
 	printf "  clidoc  Generate CLI documentation file\n"
-	printf "  sorc    Run sourcery with auto-fix\n"
 }
 
 setup() {
@@ -60,10 +59,6 @@ watch() {
 
 clidoc() {
 	uv run typer paper.gpt.cli utils docs --output src/paper/gpt/CLI.md
-}
-
-sorc() {
-	uv run sourcery review . --fix
 }
 
 if [ $# -eq 0 ]; then
