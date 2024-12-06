b0a6b438310ab53c64f2c7f3f14cc23c972cf021
Italo Silva <italo@maleldil.com>
2024-12-06 02:07:26 +0000

    peter(cli): Add command to query complete graph
    The goal is to just test the graph to see if it's working properly.

--- src/paper/peter/cli.py
+++ src/paper/peter/cli.py
@@ -10,6 +10,7 @@ import typer
 
 from paper import embedding as emb
 from paper import gpt
+from paper import semantic_scholar as s2
 from paper.peter import citations, graph, semantic
 from paper.util import Timer, display_params, setup_logging
 from paper.util.serde import load_data, save_data
@@ -159,5 +160,64 @@ def build(
     save_data(output_file, main_graph.to_data())
 
 
+@app.command(no_args_is_help=True)
+def query(
+    graph_file: Annotated[
+        Path, typer.Option("--graph", help="Path to full graph file.")
+    ],
+    ann_file: Annotated[
+        Path,
+        typer.Option(
+            "--asap-ann",
+            help="File with ASAP papers with extracted backgrounds and targets.",
+        ),
+    ],
+    title: Annotated[
+        str | None,
+        typer.Option(
+            help="Title of the paper to test query. If absent, use an arbitrary paper."
+        ),
+    ] = None,
+) -> None:
+    """Query full graph to get polarised related papers."""
+    logger.info(display_params())
 
-    app()
+    logger.debug("Loading data.")
+    ann = gpt.PromptResult.unwrap(
+        load_data(ann_file, gpt.PromptResult[gpt.ASAPAnnotated])
+    )
+    if title is None:
+        paper = ann[0]
+    else:
+        paper = next(p for p in ann if s2.clean_title(p.title) == s2.clean_title(title))
+    logger.debug("Paper selected: %s.", paper.title)
+
+    logger.debug("Loading graph.")
+    main_graph = graph.graph_from_json(graph_file)
+
+    logger.debug("Querying graph.")
+
+    time_all = 0
+    n_runs = 10
+    result = None
+    for _ in range(n_runs):
+        with Timer("Graph query") as timer:
+            result = main_graph.query_all(paper.id, paper.background, paper.target)
+        logger.info(timer)
+        time_all += timer.seconds
+
+    logger.info("Average out of %d runs: %f.", n_runs, time_all / n_runs)
+    assert result
+
+    print(title)
+    print()
+    for label, papers in [
+        (">> semantic_positive", result.semantic_positive),
+        (">> semantic_negative", result.semantic_negative),
+        (">> citations_positive", result.citations_positive),
+        (">> citations_negative", result.citations_negative),
+    ]:
+        print(f"{label} ({len(papers)})")
+        for p in papers:
+            print(f"- {p.title}")
+        print()
