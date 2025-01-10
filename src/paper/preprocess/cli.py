b18461af79b9489543b44712141341759a4ee347
Italo Silva <italo@maleldil.com>
2025-01-10 12:41:40 +0000

    s2orc: Remove package and dependencies
    S2ORC is not a good idea for us since we can use the S2 API directly,
    and it's enough.
    
    I can always revert this in the future if I want it back. For now, I'm
    removing the unused code from.

--- src/paper/preprocess/cli.py
+++ src/paper/preprocess/cli.py
@@ -3,7 +3,6 @@
 import typer
 
 from paper.peerread import preprocess as peerrread
-from paper.s2orc import preprocess as s2orc
 
 app = typer.Typer(
     name="preprocess",
@@ -14,9 +13,6 @@ app = typer.Typer(
     help="Run different preprocessing pipelines.",
 )
 
-app.command(
-    name="s2orc", help="Run S2ORC preprocessing pipeline", no_args_is_help=True
-)(s2orc.pipeline)
 app.command(
     name="peerread", help="Run PeerRead preprocessing pipeline", no_args_is_help=True
 )(peerrread.pipeline)
