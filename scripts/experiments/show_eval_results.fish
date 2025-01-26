6a5285f149b907703ed786191526127213701246
Italo Silva <italo@maleldil.com>
2025-01-26 00:40:28 +0000

    scripts(experiments): Copy json-to-table into the project
    It's small enough that I can't justify the dependency.

--- scripts/experiments/show_eval_results.fish
+++ scripts/experiments/show_eval_results.fish
@@ -1,14 +1,15 @@
 #!/usr/bin/env fish
 # Show evaluation metrics from JSON files in a pretty table.
-# json-to-table comes from https://github.com/oyarsa/scripts/tree/master/python
 
 if test -z "$argv"
     echo "Usage: show_eval_results.fish <file1> <file2> ..."
     exit 1
 end
 
+set script_dir (dirname (dirname (status filename)))
+
 for file in $argv
     jq --arg filename (basename (dirname $file)) \
         '{file: $filename} + . | del(.confusion, .correlation, .mode)' $file
 end | jq -s '. | sort_by(-.accuracy)' \
-    | ,json-to-table --fmt '{:.4f}' precision recall f1 correlation cost
+    | python $script_dir/tools/json_to_table.py --fmt '{:.4f}' precision recall f1 correlation cost
