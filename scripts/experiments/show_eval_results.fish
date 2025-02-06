da01e170f362dddd6d8853f649aac6fa30254afa
Italo Silva <italo@maleldil.com>
2025-02-06 00:01:10 +0000

    scripts(experiments): Show gold metrics on eval results
    I need the gold rating mean/stdev/median to compare with the predicted
    ones. I could print just the first one, but printing all is a
    double-check for inconsistencies.

--- scripts/experiments/show_eval_results.fish
+++ scripts/experiments/show_eval_results.fish
@@ -10,8 +10,39 @@ echo "Git commit: $(git rev-parse HEAD)"
 
 set script_dir (dirname (dirname (status filename)))
 
+# Show the evaluation metrics for each run
+echo "> Evaluation metrics"
 for file in $argv
     jq --arg filename (basename (dirname $file)) \
-        '{file: $filename} + . | del(.confusion, .correlation, .mode)' $file
+        '{
+          file: $filename,
+          precision,
+          recall,
+          f1,
+          accuracy,
+          mae,
+          mse,
+          mean: .stats_pred.mean,
+          median: .stats_pred.median,
+          stdev: .stats_pred.stdev,
+          cost,
+        }' \
+        $file
 end | jq -s '. | sort_by(-.accuracy)' \
-    | python $script_dir/tools/json_to_table.py --fmt '{:.4f}' precision recall f1 correlation cost
+    | python $script_dir/tools/json_to_table.py --fmt '{:.4f}' precision recall f1 correlation cost stdev
+
+# Show the gold metrics for each run. They should be identical, or something's wrong.
+echo
+echo "> Gold metrics"
+for file in $argv
+    jq --arg filename (basename (dirname $file)) \
+        '{
+          file: $filename,
+          accuracy,
+          mean: .stats_true.mean,
+          median: .stats_true.median,
+          stdev: .stats_true.stdev,
+        }' \
+        $file
+end | jq -s '. | sort_by(-.accuracy)' \
+    | python $script_dir/tools/json_to_table.py --fmt '{:.4f}' stdev
