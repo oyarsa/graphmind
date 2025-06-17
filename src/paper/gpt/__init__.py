9981f056211fd839250698900ac09ff443bd0cc9
Italo Silva <italo@maleldil.com>
2025-06-17 14:54:54 +0100

    gpt: Export GPTResult

--- src/paper/gpt/__init__.py
+++ src/paper/gpt/__init__.py
@@ -20,5 +20,6 @@ from paper.gpt.model import PeerReadAnnotated as PeerReadAnnotated
 from paper.gpt.model import RelatedPaperSource as RelatedPaperSource
 from paper.gpt.model import is_rationale_valid as is_rationale_valid
 from paper.gpt.prompts import PromptTemplate as PromptTemplate
+from paper.gpt.run_gpt import GPTResult as GPTResult
 from paper.gpt.run_gpt import PromptResult as PromptResult
 from paper.gpt.run_gpt import count_tokens as count_tokens
