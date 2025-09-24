3b50b475c8166f3ef77ec5ac983c53ad85fcef37
Italo Silva <italo@maleldil.com>
2025-09-24 13:36:38 +0100

    scripts(exp): Add total cost to evaluation results

--- scripts/experiments/show_eval_results.fish
+++ scripts/experiments/show_eval_results.fish
@@ -11,7 +11,7 @@ echo "Git commit: $(git rev-parse HEAD)"
 set script_dir (dirname (dirname (status filename)))
 
 # Show the evaluation metrics for each run
-echo "> Evaluation metrics"
+echo
 for file in $argv
     jq --arg filename (basename (dirname $file)) \
         '{
@@ -26,4 +26,9 @@ for file in $argv
         }' \
         $file
 end | jq -s '. | sort_by(-.accuracy)' \
-    | python $script_dir/tools/json_to_table.py --fmt '{:.4f}' precision recall f1 correlation cost
+    | python $script_dir/tools/json_to_table.py \
+    --fmt '{:7.4f}' precision recall f1 correlation cost confidence accuracy
+
+# Calculate total cost across all files
+set total_cost (for file in $argv; jq -r '.cost // 0' $file; end | jq -s 'add')
+printf '\nTotal cost: $%.4f\n' $total_cost
