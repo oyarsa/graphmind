e1526ec99ae75916e196b1940b6a61fc40391c0b
Italo Silva <italo@maleldil.com>
2026-02-14 01:24:29 +0000

    util: Fix comment on COLOURS - INFO has no colour

--- src/paper/util/__init__.py
+++ src/paper/util/__init__.py
@@ -149,7 +149,7 @@ class ColoredFormatter(logging.Formatter):
 
     COLORS: Mapping[str, str] = {
         "DEBUG": "\033[36m",  # Cyan
-        "INFO": "\033[0m",  # Green
+        "INFO": "\033[0m",  # No colour
         "WARNING": "\033[33m",  # Yellow
         "ERROR": "\033[31m",  # Red
         "CRITICAL": "\033[41m",  # Red background
